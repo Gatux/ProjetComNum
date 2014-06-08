9127ca9723b5176c5bb1f93c2ec7c07f398b15cb
RainbowLyte <rainbowlyte42@gmail.com>
2014-06-08 16:40:14 +0200

    Q 4.2.1.2

--- main_QPSK_v1.m
+++ main_QPSK_v1.m
@@ -27,6 +27,7 @@ F0 = 2500;      % Fréquence de la porteuse
 Tg = 4*Ts;      % Temps de propagation de groupe
 alpha = 0.5;    % Facteur de roll-off
 Ns = 5000;      % Nombre de symboles par paquet
+nbPkt = 1;      % Nombre de paquets
 Fse = Ts*Fe;    % Facteur de sur-échantillonnage
 N = 512;        % Nombre de points sur lesquels sont les TF sont faites
 M = 4;          % Nombre de symboles dans la constellation
@@ -38,21 +39,25 @@ sigma = 0;      % Variance
 %% 1. Tracé de la réponse impulsionnelle et du module de la fonction de transfert du filtre en racine de cosinus sur-élevé
 
 % Fonction de transfert
-g = rcosfir(alpha, 1, [], Ts, 'sqrt');
+g = rcosfir(alpha, 4, Fse, Ts, 'sqrt');
 
 figure(1)
+
 subplot 211
 impz(g);        % Réponse du filtre à un dirac
+title('Réponse impulsionnelle de g(t)');
+
 hold on;
+
 subplot 212
 plot(abs(g))    % Module de la fonction de transfert
+title('Module de la fonction de transfert de g(t)');
 
 
 %% 2. Implémentation d'un modulateur et d'un démodulateur
 
 pkt = randi([0 1], Ns, M/2);            % Création d'un paquet contenant les bits à encoder en symboles
 symb_tx = bi2de(pkt)';                  % Conversion bits->symbole
-
 
 % Modulateur
 s_s = pskmod(symb_tx, M, pi/4);
@@ -64,33 +69,37 @@ symb_rx = pskdemod(s_s, M, pi/4);
 %% 3. Vérification du fonctionnement de la chaîne de communication en BDB
 
 % Association bits->symbole
-pkt = randi([0 1], Ns, M/2);            % Création d'un paquet contenant les bits à encoder en symboles
-symb_tx = bi2de(pkt)';                  % Conversion bits->symbole
+pkt_tx = randi([0 1], Ns * nbPkt, M/2);             % Création d'un paquet contenant les bits à encoder en symboles
+symb_tx = bi2de(pkt_tx)';                           % Conversion bits->symbole
+pkt_tx = pkt_tx';
+pkt_tx = pkt_tx(:)';
 
 % Modulateur
-phase_tx = pskmod(symb_tx, M, pi/4)
-s_s = real(phase_tx)
-
-% % Génération du signal s_s
-% t = 0:Ts;
-% carrier = exp(i*2*pi/Ts*t)
-% s_s = phase_tx'*carrier;
-% s_s = s_s';
-% s_s = s_s(:)';
+phase_tx = pskmod(symb_tx, M, pi/4);
+s_s = upsample(phase_tx, Fse);
 
 % Filtre de mise en forme
-g = rcosfir(alpha, Tg/Ts, [], Ts, 'sqrt');
+g = rcosfir(alpha, 4, Fse, Ts, 'sqrt');
 s_l = conv(s_s, g);
+
+% Canal
+y_l = s_l + (mu + sigma * randn(1, length(s_l)));
 
 % Filtre de réception
 r_l = conv(s_l, g);
 
 % Echantillonnage au rythme Ts
-% r_ln = downsample(r_l, 1);
-% plot(r_ln)
+r_ln = downsample(r_l, Fse);
 
-% symb_tx
-% symb_rx = pskdemod(r_l, M, pi/4)
-% length(symb_rx)
-% length(symb_tx)
-% sum( (symb_rx == symb_tx) == 0 )
+% Démodulateur
+symb_rx = pskdemod(r_ln, M, pi/4);
+symb_rx = symb_rx(9:end-8);
+
+% Décision
+pkt_rx = de2bi(symb_rx)';
+pkt_rx = pkt_rx(:)';
+
+disp('Taux d''Erreur Binaire : ');
+disp(sum((pkt_rx == pkt_tx)==0));
+
+
